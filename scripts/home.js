11a868a9c31fb00e179023a2ff27abd8c6a397c2
Propha <gauthier.pellegrin@gmail.com>
2018-05-18 16:42:31 +0200

    initial project commit

---
+++ scripts/home.js
@@ -0,0 +1,10 @@
+// manage Home button
+let homeButton = document.getElementById("homeButton");
+
+homeButton.addEventListener('click', function(event){
+    window.location.href="index.html"
+});
+
+
+
+
